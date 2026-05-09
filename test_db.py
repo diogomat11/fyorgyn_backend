b8e3f14e074ab388444e0b323c532048cf35fe7c
baldurokkprojetos-clients <baldurokkprojetos-clients@users.noreply.github.com>
2026-05-09 14:22:34 -0300

    chore: remove test and migration python scripts

--- test_db.py
+++
@@ -1,6 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    res = conn.execute(text('SELECT id_lote, "detalheId" FROM faturamento_lotes LIMIT 5')).fetchall()
-    print("Faturamento Lotes sample:", res)
